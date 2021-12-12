46298f7ce572cff573f0910785e31f0c301f6f7d
Austin Ahlers <aahlers01@gmail.com>
2021-12-12 15:53:33 -0700

    Update to Architecture
    Database now includes both player and team models with capacity for player free agency

--- main.py
+++ main.py
@@ -12,18 +12,117 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-# This is our model for a database table construction
+# This is our Team model for database player table construction
+class TeamModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), nullable=False)
+    owner = db.Column(db.String(50), nullable=False)
+    year_formed = db.Column(db.Integer, nullable=False)
+    num_players = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return f"Team(name = {name}, owner = {owner}, num_players = {num_players})"
+
+# This request parser tells us what arguments are required in any player data given
+#   This specific one is for put, if you are writing an update method where 
+#   you dont need all of these arguments, you should write another parser 
+#   for that method below
+team_put_args = reqparse.RequestParser()
+team_put_args.add_argument(
+    "name", type=str, help="Name of the Team is required", required=True)
+team_put_args.add_argument(
+    "owner", type=str, help="Owner of the team is required", required=True)
+team_put_args.add_argument(
+    "year_formed", type=int, help="The year the team was formed is required", required=True)
+
+team_patch_args = reqparse.RequestParser()
+team_patch_args.add_argument(
+    "name", type=str, help="Name of the team")
+team_patch_args.add_argument(
+    "owner", type=str, help="Owner of the team")
+
+# This dict is used as our benchmark for how marshal_with will format our team records
+team_resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "owner": fields.String,
+    "year_formed": fields.Integer,
+    "num_players": fields.Integer
+}
+
+# The Team resource we are accessing
+class Team(Resource):
+
+    @marshal_with(team_resource_fields)
+    def get(self, team_id):
+        result = TeamModel.query.filter_by(id=team_id).first()
+        if result is None:
+            abort(404, message="Team with given ID does not exist")
+        return result
+
+    @marshal_with(team_resource_fields)
+    def put(self, team_id):
+        args = team_put_args.parse_args()
+        result = TeamModel.query.filter_by(id=team_id).first()
+        if result:
+            abort(409, message="Team with given team ID already exists")
+        team = TeamModel(id=team_id, 
+                         name=args['name'],
+                         owner=args['owner'],
+                         year_formed=args['year_formed'],
+                         num_players=0)
+        db.session.add(team)
+        db.session.commit()
+        return team, 201
+
+    @marshal_with(team_resource_fields)
+    def patch(self, team_id):
+        args = team_patch_args.parse_args()
+        team_record = TeamModel.query.filter_by(id=team_id).first()
+        if team_record is None:
+            abort(404, message="Team with given ID does not exist...")
+        if args['name'] is not None:
+            # If the name of the team has changed
+            #   find all of its players and update the team they play for with the new name
+            #   then update the name of the team to the new name
+            player_records = PlayerModel.query.filter_by(team=team_record.name).all()
+            for i in range(len(player_records)):
+                player_records[i].team = args['name']
+            team_record.name = args['name']
+        if args['owner'] is not None:
+            team_record.owner = args['owner']
+        db.session.commit()
+        return team_record, 200
+
+    def delete(self, team_id):
+        team_record = TeamModel.query.filter_by(id=team_id).first()
+
+        # If team does not exist
+        if team_record is None:
+            abort(404, message="Team with given ID does not exist")
+        
+        # Find all the players on the former team and make them free agents
+        player_records = PlayerModel.query.filter_by(team=team_record.name).all()
+        for i in range(len(player_records)):
+            player_records[i].team = "Free Agent"
+
+        db.session.delete(team_record)
+        db.session.commit()
+        return "", 200
+    
+
+# This is our Player model for database player table construction
 class PlayerModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    team = db.Column(db.String(50), nullable=False)
+    team = db.Column(db.String(50), nullable=True)
     age = db.Column(db.Integer, nullable=False)
     position = db.Column(db.String(2), nullable=False)
 
     def __repr__(self):
-        return f"Players(name = {name}, team = {team}, position = {position})"
+        return f"Player(name = {name}, team = {team}, position = {position})"
 
-# This request parser tells us what arguments are required in any data given
+# This request parser tells us what arguments are required in any player data given
 #   This specific one is for put, if you are writing an update method where 
 #   you dont need all of these arguments, you should write another parser 
 #   for that method below
@@ -31,14 +130,24 @@ players_put_args = reqparse.RequestParser()
 players_put_args.add_argument(
     "name", type=str, help="Name of the player is required", required=True)
 players_put_args.add_argument(
-    "team", type=str, help="Team of the player is required", required=True)
+    "team", type=str, help="Team of the player (if the player is a free agent put 'Free Agent')", required=False)
 players_put_args.add_argument(
     "age", type=int, help="Age of the player is required", required=True)
 players_put_args.add_argument(
     "position", type=str, help="Position of the player is required", required=True)
 
-# This dict is used as our benchmark for how marshal_with will format our records
-resource_fields = {
+players_patch_args = reqparse.RequestParser()
+players_patch_args.add_argument(
+    "name", type=str, help="Name of the player")
+players_patch_args.add_argument(
+    "team", type=str, help="Team of the player (if the player is a free agent put 'Free Agent')")
+players_patch_args.add_argument(
+    "age", type=int, help="Age of the player")
+players_patch_args.add_argument(
+    "position", type=str, help="Position of the player")
+
+# This dict is used as our benchmark for how marshal_with will format our player records
+player_resource_fields = {
     "id": fields.Integer,
     "name": fields.String,
     "team": fields.String,
@@ -46,24 +155,36 @@ resource_fields = {
     "position": fields.String
 }
 
-# The resource we are accessing
+# The Player resource we are accessing
 class Player(Resource):
 
     # The overwriting of the get method in flask with a mw decorator
-    @marshal_with(resource_fields)
+    @marshal_with(player_resource_fields)
     def get(self, player_id):
-        result = PlayerModel.query.filter_by(id=player_id).first()
-        if not result:
+        player_record = PlayerModel.query.filter_by(id=player_id).first()
+        if not player_record:
             abort(404, message="Player with given ID does not exist")
-        return result
+        return player_record
 
     # The overwriting of the put method in flask with a mw decorator
-    @marshal_with(resource_fields)
+    @marshal_with(player_resource_fields)
     def put(self, player_id):
         args = players_put_args.parse_args()
-        result = PlayerModel.query.filter_by(id=player_id).first()
-        if result:
+        player_record = PlayerModel.query.filter_by(id=player_id).first()
+
+        # If their is an existing player record
+        if player_record:
             abort(409, message="Player with given ID already exists...")
+
+        team_record = TeamModel.query.filter_by(name=args['team']).first()
+
+        # If the player isnt a free agent and the team doesnt exist
+        if args['team'] != "Free Agent" and team_record is None:
+            abort(404, message="Player cannot play for a team that doesnt exist...")
+
+        # If the player isnt a free agent but the team exists
+        if args['team'] != "Free Agent":
+            team_record.num_players += 1
         player = PlayerModel(id=player_id,
                              name=args['name'],
                              team=args['team'],
@@ -73,19 +194,65 @@ class Player(Resource):
         db.session.commit()
         return player, 201
 
-    """
-    def delete(self, player_id):
-        abort_if_invalid_player(player_id)
-        del players[player_id]
-        return "", 204
-    """
+    # Works perfectly in tandem with team resource, per team player count, and free agency
+    @marshal_with(player_resource_fields)
+    def patch(self, player_id):
+        result = PlayerModel.query.filter_by(id=player_id).first()
+        if result is None:
+            abort(404, message="Player with given ID does not exist")
+        args = players_patch_args.parse_args()
+        if args['name'] is not None:
+            result.name = args['name']
+        if args['team'] is not None:
+            # Find the records for the old and new teams
+            old_team_record = TeamModel.query.filter_by(name=result.team).first()
+            new_team_record = TeamModel.query.filter_by(name=args['team']).first()
 
+            # Check if new team exists
+            if new_team_record is None:
+                abort(404, message="Player cannot play for a team that doesnt exist...")
+
+            # Subtract player from old team if the player is not a free agent
+            if old_team_record:
+                old_team_record.num_players -= 1
+
+            # Add player to new team
+            new_team_record.num_players += 1
+
+            # Set players team to new team
+            result.team = args['team']
+        if args['age'] is not None:
+            result.age = args['age']
+        if args['position'] is not None:
+            result.position = args['position']
+        db.session.commit()
+        return result, 200
+
+    # Works perfectly in tandem with team resource, per team player count, and free agency
+    def delete(self, player_id):
+        result = PlayerModel.query.filter_by(id=player_id).first()
+        if result is None:
+            abort(404, message="Player with the given ID does not exist...")
+
+        team_record = TeamModel.query.filter_by(name=result.team).first()
+
+        # If the player is not a free agent
+        if team_record:
+            team_record.num_players -= 1
+
+        db.session.delete(result)
+        db.session.commit()
+        return "", 204
+
+# Comment and uncomment this line as needed in order to add additional resources
+#db.create_all()
 
 # Here is where we add all of our resources
 #   We must given flask the name of the resource
 #   and the path of the resource at the end of the base URI.
 #   For this resource we are saying the user must enter player, followed by some player ID
 api.add_resource(Player, "/player/<int:player_id>")
+api.add_resource(Team, "/team/<int:team_id>")
 
 # Here is where we run the server
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,29 +4,80 @@ from requests.api import request
 # This is the http address for the local machine we are running on
 BASE_URI = "http://127.0.0.1:5000/"
 
-RESOURCE_URI = "player/"
+# URI extensions for the different resources
+TEAM_RESOURCE_URI = "team/"
+PLAYER_RESOURCE_URI = "player/"
+
+# team data to be added to database
+team_data = [
+    {"name": "Broncos", "owner": "The Bowlen Family", "year_formed": 1960},
+    {"name": "Cheifs", "owner": "Clark Hunt", "year_formed": 1960},
+    {"name": "Chargers", "owner": "Dean Spanos", "year_formed": 1960},
+    {"name": "Raiders", "owner": "Mark Davis", "year_formed": 1960}
+]
 
 # player data to be added to database
-data = [
+player_data = [
     {"name": "Austin Ahlers", "team": "Broncos", "age": 21, "position": "DE"},
     {"name": "Carson Ahlers", "team": "Broncos", "age": 13, "position": "WR"},
     {"name": "Cooper Ahlers", "team": "Cheifs", "age": 11, "position": "QB"},
-    {"name": "Matthew Ahlers", "team": "Cowboys", "age": 49, "position": "DT"},
-    {"name": "John Doe", "team": "Washington Football Team", "age": 34, "position": "RB"},
+    {"name": "Matthew Ahlers", "team": "Chargers", "age": 49, "position": "DT"},
+    {"name": "John Doe", "team": "Free Agent", "age": 34, "position": "RB"},
 ]
 
+# PUT requests to add teams to database
+# and print statements for seeing the responses from our API
+for i in range(len(team_data)):
+    response = requests.put(BASE_URI + TEAM_RESOURCE_URI + str(i), team_data[i])
+    print(response.json())
 
 # PUT requests to add players to database
 # and print statements for seeing the responses from our API
-for i in range(len(data)):
-    response = requests.put(BASE_URI + RESOURCE_URI + str(i), data[i])
+for i in range(len(player_data)):
+    response = requests.put(BASE_URI + PLAYER_RESOURCE_URI + str(i), player_data[i])
     print(response.json())
 
+broncos_patch_data = {
+    "name": "Denver Sports Team",
+    "owner": "The Ahlers Family"
+    }
 
-response = requests.get(BASE_URI + RESOURCE_URI + "20")
+response = requests.patch(BASE_URI + TEAM_RESOURCE_URI + "0", broncos_patch_data)
 print(response.json())
 
 """
+responee = requests.delete(BASE_URI + PLAYER_RESOURCE_URI + "4")
+print(response)
+
+responee = requests.delete(BASE_URI + PLAYER_RESOURCE_URI + "2")
+print(response)
+"""
+
+responee = requests.delete(BASE_URI + TEAM_RESOURCE_URI + "0")
+print(response)
+
+"""
+response = requests.get(BASE_URI + TEAM_RESOURCE_URI + "0")
+print(response.json())
+
+"""
+
+"""
+response = requests.delete(BASE_URI + PLAYER_RESOURCE_URI + "2")
+print(response)
+
+response = requests.get(BASE_URI + PLAYER_RESOURCE_URI + "20")
+print(response.json())
+
+cooper_patch = {
+    "name": "Cooper Matthew Ahlers",
+    "position": "TE", 
+    "team": "Broncos"
+}
+
+response = requests.patch(BASE_URI + PLAYER_RESOURCE_URI + "2", cooper_patch)
+print(response.json())
+
 # Here we get the response from the API when we send a get request using the whole URL
 #response = requests.get(BASE_URI + RESOURCE_URI)
 response = requests.put(BASE_URI + RESOURCE_URI)
